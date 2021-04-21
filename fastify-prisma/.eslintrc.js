311b809b95ae077211115e7c0028ea666633d3dd
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-04-21 09:52:16 +0200

    test(fastify-prisma): setup jest testing

--- fastify-prisma/.eslintrc.js
+++ fastify-prisma/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', '*.config.js', '*.json', 'test', 'seed.ts'],
+  ignorePatterns: ['.eslintrc.js', '*.config.js', '*.json', 'test', 'seed.ts', '*.setup.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
---
+++ fastify-prisma/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: './',
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  },
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  setupFiles: ['./jest.setup.ts'],
+};
